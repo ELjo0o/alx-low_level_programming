67bdd654cf1eb2ec99c183c027701c3b86e3d5ce
ELjo0o <youcifer.777@gmail.com>
2023-06-17 01:56:01 +0300

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<ctype.h>
+
+/**
+*main - Prints all combinations of two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
+
+
+int main(void)
+{
+	int first, second;
+
+	for (first = 0; first <= 9 first++)
+	{
+		for (second = 1; second <= 10; second++)
+		{
+
+			putchar((first % 10) + '0');
+			putchar((second % 10) + '0');
+
+			if (first == 8 && second == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+
+		}
+
+
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
