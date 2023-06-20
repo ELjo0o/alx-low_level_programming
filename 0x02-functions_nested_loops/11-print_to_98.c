45ef35f01ff5cb0c40bfd2b0182a18579593b4f4
ELjo0o <youcifer.777@gmail.com>
2023-06-21 01:19:17 +0300

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_to_98 - print number from 0 to 98.
+* @n:  number to print.
+* Return: (0)
+*/
+
+
+void print_to_98(int n)
+{
+	int n;
+
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(n)
+		_putchar(',')
+		_putchar(' ')
+	}
+
+	_putchar('\n')
+}
