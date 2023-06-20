e8474fedae162efba48f8866b0f19f319e0b92a7
ELjo0o <youcifer.777@gmail.com>
2023-06-20 19:50:46 +0300

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry point.
+* DESC - print ten time alphabet.
+* Return - (0) (Success).
+*/
+
+void print_alphabet_x10(void)
+{
+	int line;
+	char alpha;
+
+	for (line = '0'; line = <= 9; line++)
+	{
+		_putchar(line);
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	}
+
+	_putchar('\n')
+}
