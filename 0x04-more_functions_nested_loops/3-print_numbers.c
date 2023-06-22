3a8eea8bdd105e1d7e19e9f551092773c0becea2
ELjo0o <youcifer.777@gmail.com>
2023-06-22 18:23:45 +0300

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
 #include "main"
-#include<stdio.h>
+
 
 /**
 *print_numbers - prints the numbers, from 0 to 9, followed by a new line.
@@ -10,7 +10,7 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = 48; num < 58; num++)
+	for (num = 48, num < 58; num++)
 	{
 		_putchar(num);
 	}
