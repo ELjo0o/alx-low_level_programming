5d2ba107c2b2603ba8568beb57277e69722b1103
ELjo0o <youcifer.777@gmail.com>
2023-06-23 02:06:59 +0300

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+*print_triangle - prints a triangle
+*@size:size parameter of triangle
+*Return: (0).
+*/
+
+void print_triangle(int size)
+{
+	int x1, x2;
+
+	if (size > 0)
+	{
+		for (x1 = 1; x1 <= size; x1++)
+		{
+			for ((x2 = size - x1); x2 > 0; x2--)
+			{
+				_putchar(' ');
+			}
+
+			for (x2 = 0; x2 < x1; x2++)
+			{
+				_putchar('#');
+			}
+
+			if (x1 == size)
+			{
+				continue;
+			}
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+*main - program that prints the numbers from 1 to 100, followed by a new line.
+*But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz.
+* For numbers which are multiples of both three and five print FizzBuzz.
+*Return: (0).
+*/
+
+int main(void)
+{
+	int x1, x2, x3;
+
+	
+
+
+}
