e54347afa4ce0689b53b8369259506533479646c
ELjo0o <youcifer.777@gmail.com>
2023-06-26 17:51:32 +0300

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 *_puts - a function that prints a string, followed by a new line, to stdout.
 *@str: the string.
@@ -12,9 +11,9 @@ void _puts(char *str)
 {
 	int count = 0;
 
-	while (str[count] != '\0')
+	while (*str[count] != '\0')
 	{
-		_putchar(str[count]);
+		_putchar(*str[count]);
 		count++;
 	}
 	_putchar('\n');
