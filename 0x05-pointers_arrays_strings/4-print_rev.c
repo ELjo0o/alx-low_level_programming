c5c0ffcf5cb05a673232cd0b49548d54cd3d78c9
ELjo0o <youcifer.777@gmail.com>
2023-06-27 02:11:47 +0300

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,9 +5,20 @@
 *@s: the char.
 *Return: (0).
 */
+
 void print_rev(char *s)
 {
-	int base = *s;
-	print_rev(base);
-	return (0);
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
