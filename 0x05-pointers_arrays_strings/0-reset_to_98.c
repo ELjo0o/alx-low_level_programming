53c71da85b34e0cdb39cdd60a5c5a6824d9a24f8
ELjo0o <youcifer.777@gmail.com>
2023-06-26 16:28:05 +0300

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+*reset_to_98 - function that takes a pointer to an int as parameter
+*and updates the value it points to to 98.
+*@n: number of pointer.
+*Return: (0).
+*/
+
+
+void reset_to_98(int *n)
+{
+	int *n;
+
+	if (*n == 98)
+	{
+		_putchar("%d\n", *n);
+	}
+	else
+	{
+		_putchar("%d\n", *n);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef _MATH_H_
+#define _MATH_H_
+
+
+
+void reset_to_98(int *n);
+
+
+
+#endif
