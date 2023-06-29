02a93d891e36fbd157b2c13745343d45cf3f56f2
ELjo0o <youcifer.777@gmail.com>
2023-06-30 02:24:56 +0300

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int result;
+	int i = 0, res = 0;
 
-	result = strcmp(*s1, *s2);
-	return (result);
+	while (res == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		res = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (res);
 }
