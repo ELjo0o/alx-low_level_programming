54d485e73b96bdf7e3d305372380f96b1c48a688
ELjo0o <youcifer.777@gmail.com>
2023-06-30 02:30:10 +0300

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * @a: an array of integers.
+ * @n: the number of elements to swap.
+ * Return: nothing.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n[i]; i++)
+	{
+		_putchar(i);
+	}
+}
