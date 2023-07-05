59a29fd2cb261e956bc37e93dfdb7902ef412a68
ELjo0o <youcifer.777@gmail.com>
2023-07-05 17:31:53 +0300

    C - Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,8 +8,17 @@
 
 int factorial(int n);
 {
+	int fac;
+
 	if (n < 0)
 	{
-		return (n -1);
+		return (-1);
 	}
+	else if
+	{
+		(n == 0)
+		return (1);
+	}
+	fac = facrorial(n -1);
+	return(n * fac);
 }
