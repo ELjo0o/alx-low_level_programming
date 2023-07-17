c2c6d31c44c53d695def118edb0e10556de5263f
ELjo0o <youcifer.777@gmail.com>
2023-07-17 15:31:22 +0300

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
 
+/**
+* struct dog - a structures.
+* @name: dog's name.
+* @age: dog's age.
+* @owner: owner's name.
+* DES: just for dog struct
+*/
 
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 }
+
+#endif
