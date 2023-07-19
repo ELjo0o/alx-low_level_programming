d644961567d8227ac235c54012b625e374ff7398
ELjo0o <youcifer.777@gmail.com>
2023-07-19 16:46:24 +0300

    C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a name
-* @name:name to print
-* @f: function which print name
-*/
+ * print_name - prints a name.
+ * @name: the name to be printed to stdout
+ * @f: a function pointer that determines the format of name
+ * Return: nothing
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
-
 	f(name);
 }
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,7 +5,9 @@
 #define FUNCTION_POINTERS
 
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
+int _putchar(char c);
+void array_iterator(int *array, int size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif 
+
+#endif
