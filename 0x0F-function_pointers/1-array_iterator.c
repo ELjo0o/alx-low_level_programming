d8f661eb21466a7c6c2b58fb3fd8885cbba7f697
ELjo0o <youcifer.777@gmail.com>
2023-07-19 17:21:46 +0300

    C - Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,18 +5,14 @@
 * @array: array to be targeted
 * @size: size of array
 * @action: function to be executed
-*
+* Return: void.
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int *end = array + size - 1;
 
-	if (array == NULL || action == NULL)
-		return;
-
-	while (size-- > 0)
-	{
-		action(*array);
-		array++;
-	}
+	if (array && size && action)
+		while (array <= end)
+			action(*array++);
 }
